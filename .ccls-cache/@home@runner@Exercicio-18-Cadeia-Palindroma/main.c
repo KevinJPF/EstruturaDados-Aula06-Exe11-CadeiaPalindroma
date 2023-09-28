e9f1986d48a5054518b24eaff4d3eda636704667
KevinJPF <123981519+KevinJPF@users.noreply.github.com>
2023-09-28 13:38:06 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@Exercicio-18-Cadeia-Palindroma/main.c
@@ -0,0 +1,28 @@
+#include <ctype.h>
+#include <stdio.h>
+#include <string.h>
+
+int inv(char s[100], int p, int u) {
+  if (p >= u)
+    return 0;
+
+  if (!isalpha(toupper(s[p])))
+    p++;
+  if (!isalpha(toupper(s[u])))
+    u--;
+
+  if (toupper(s[p]) == toupper(s[u]))
+    return inv(s, p + 1, u - 1) + 0;
+  else
+    return inv(s, p + 1, u - 1) + 1;
+}
+
+int main(void) {
+  char s[100] = "A base do teto desabou";
+
+  if (inv(s, 0, strlen(s)) == 0)
+    printf("A frase '%s' e palindroma.", s);
+  else
+    printf("A frase '%s' nao e palindroma.", s);
+  return 0;
+}
